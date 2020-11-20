a2948e68ee0a16be74975bf2f6400c4088e2b224
ferangmor <ferangmor@alum.us.es>
2020-11-20 15:29:38 +0100

    Event try

---
+++ src/main/java/org/springframework/samples/petclinic/model/Event.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Event extends NamedEntity {
+	
+	private String description;
+	
+	       
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate start;
+	
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private LocalDate end;
+}
