6dc8838745b9ce628cf2d401b55f6a8cb51d53e8
horgarler <73118504+horgarler@users.noreply.github.com>
2020-11-29 23:25:40 +0100

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -64,12 +64,12 @@ INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04',
 
 
 
-INSERT INTO camarero(id,name,apellido,gmail,telefono,contrasena) VALUES (1, 'Victor', 'El Tito', 'eltitovictor@gmail.com',6464646464,'12345');
+INSERT INTO camarero(id,name,apellido,gmail,telefono,usuario,contrasena) VALUES (1, 'Victor', 'El Tito', 'eltitovictor@gmail.com',6464646464,'Bones','12345');
 INSERT INTO cocinero(id,name,apellido,gmail,telefono,usuario,contrasena) VALUES (1, 'Horacio', 'Garcia', 'horgarler@gmail.com',6969696969,'Hori','qwerty');
 INSERT INTO producto(id,name,fecha_caducidad,cantidad_minima,cantidad_actual,cantidad_maxima) VALUES (1,'Pan', '2020-01-01', 2, 5, 10);
 INSERT INTO proveedor(id,name,apellido,gmail,telefono) VALUES (1, 'taburete', 'El capo', 'eltitocapo@gmail.com',646464664);
-INSERT INTO manager(id,name,apellido,gmail,telefono,contrasena) VALUES (2, 'Alexander', 'El Tribandera', 'tribanderaAlexito@gmail.com', 678431588, '12345');
-INSERT INTO propietario(id,name,apellido,gmail,telefono,contrasena) VALUES (1, 'Abdel', 'Ch', 'Abdch@gmail.com', 602354622, '12345');
+INSERT INTO manager(id,name,apellido,gmail,telefono,usuario,contrasena) VALUES (2, 'Alexander', 'El Tribandera', 'tribanderaAlexito@gmail.com', 678431588,'Alex', '12345');
+INSERT INTO propietario(id,name,apellido,gmail,telefono,usario,contrasena) VALUES (1, 'Abdel', 'Ch', 'Abdch@gmail.com', 602354622,'Abd', '12345');
 INSERT INTO plato(id,name,precio) VALUES (1, 'Albondigas', 2);
 
 
