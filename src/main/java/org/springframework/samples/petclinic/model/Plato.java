f7bf8be33e864731985f294aca0b180cc360ec21
ferangmor <ferangmor@alum.us.es>
2020-12-21 13:01:18 +0100

    Comandas y platos inicio

--- src/main/java/org/springframework/samples/petclinic/model/Plato.java
+++ src/main/java/org/springframework/samples/petclinic/model/Plato.java
@@ -1,12 +1,42 @@
 package org.springframework.samples.petclinic.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "platos")
 public class Plato extends NamedEntity{
+	 
+	
+	@Column(name = "precio")
+	private Integer precio;
+	
+	@Column(name = "disponible")
+	private Boolean disponible;
+	
+	protected void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+	
+	public Integer getPrecio() {
+		return this.precio;
+	}
+	
+	public void setdisponible(Boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+	public Boolean getdisponible() {
+		return this.disponible;
+	}
+	
+	
 	
 	
 }
